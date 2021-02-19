c66a9f5c36f9731b1b06922fa84f55fa987767b0
MarkelCA <cuestaarribas.markel@gmail.com>
2021-02-19 04:32:26 +0100

    automatized card creation from the robots.js object

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 //import React from 'react';
 import 'tachyons';
-const Card = ({name, email, id}) => {
+const Card = ({name, email, id, username}) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?100x100`} alt="robots" />
-            <h2>{name}</h2>
+            <h2>@{username}</h2>
+            <h3><em>{name}</em></h3>
             <p>{email}</p>
         </div>
     );
+
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import Card from './components/Card.js';
 import reportWebVitals from './reportWebVitals.js';
-import { robots } from './robots';
+import { robots } from './robots.js';
+
 ReactDOM.render(
     <React.StrictMode> 
-        <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        <div className="cards-wrapper tc bg-dark-green">
+            {robots.map(printCard)}
         </div>
     </React.StrictMode>,
   document.getElementById('root')
 );
 
+function printCard({ id, name, email, username }) {
+    return <Card key={id} id={id} name={name} email={email} username={username} />
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
