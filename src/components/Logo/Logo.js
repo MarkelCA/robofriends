fb9ae9138b17fd27e50a1940baa4fe6f424b209b
MarkelCA <cuestaarribas.markel.com>
2021-06-14 17:50:35 +0200

    changed manifest.json and public images

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 const Logo = () => {
        return <div id="main-logo-container">
-            <img id='main-logo' src={process.env.PUBLIC_URL + '/Markel.png'} alt="markel"/>
+            <img id='main-logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="markel"/>
         </div>
 }
 
